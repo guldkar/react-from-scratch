8fe0834c011ee4bece125cb7878311f45fc2de13
Johan <jka@mjolner.dk>
2023-03-15 13:59:23 +0100

    Initialized react without cra with typescript and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    port: '5000',
+    static: {
+      directory: __dirname + '/public'
+    },
+    open: true,
+    hot: true,
+    liveReload: true
+  }
+};
