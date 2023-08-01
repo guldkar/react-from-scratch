718d1866d7d222c404df8216e7bfd4169acccecf
Johan <jka@mjolner.dk>
2023-08-01 15:08:08 +0200

    Added icon to button opening window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './app.css';
+import {FaUser} from 'react-icons/fa'
 
 import { useAppSelector, useAppDispatch } from 'hooks/State';
 import { open } from 'state/WindowSlice';
@@ -17,13 +18,13 @@ const App = () => {
             <div className='p-2'>
                 <button
                     onClick={() => dispatch(open())}
-                    className='rounded bg-teal-500 text-white px-4 py-2 text-sm font-semibold shadow-sm'>
-                    open window
+                    className='rounded text-white px-4 py-2 text-sm font-semibold shadow-sm hover:border border-slate-500 '>
+                   <FaUser className='w-10 h-10'/>
+                   <div>Profile</div>
                 </button>
                 {windows.map((win) => (
                     <WindowBase key={win.id} id={win.id}>
-                        {' '}
-                        <Info />{' '}
+                        <Info />
                     </WindowBase>
                 ))}
             </div>
