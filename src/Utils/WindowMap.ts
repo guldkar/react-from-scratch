530f8717bb4b41d1dc50c3337ff14b7412a59fd7
Johan <jka@mjolner.dk>
2023-08-02 10:27:27 +0200

    Added types and an item to the windowMap

--- src/Utils/WindowMap.ts
+++ src/Utils/WindowMap.ts
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import Info from "../components/Info"
+import Info from "../components/windows/Info"
 import { WindowTypesEnum } from "../types/Enums"
+import MjolnerExperience from '../components/windows/MjolnerExperience';
 
 const WindowMap = new Map<WindowTypesEnum, JSX.Element>([
   [WindowTypesEnum.Profile, Info()],
-
+  [WindowTypesEnum.MjolnerExperience, MjolnerExperience()]
 ])
 
 export default WindowMap;
---
+++ src/types/types.d.ts
@@ -0,0 +1,8 @@
+
+export type TimelineItem = {
+  title: string;
+  start: Date;
+  end: Date;
+  Technologies: string[];
+  description: string;
+}
