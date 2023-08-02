bbac011702a7da7783c3b2ff7baf203661561560
Johan <jka@mjolner.dk>
2023-08-02 10:32:11 +0200

    Handled new folder structure, and inserted map as window displayer

---
+++ src/components/windows/MjolnerExperience.tsx
@@ -0,0 +1,13 @@
+import TimeLine from '../Timeline';
+import React from 'react';
+
+const MjolnerExperience = () => {
+    return (
+        <>
+            <h2>Projekter i Mjølnerregi</h2>
+            <TimeLine />
+        </>
+    );
+};
+
+export default MjolnerExperience;
